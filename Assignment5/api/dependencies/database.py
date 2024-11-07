60572bb63421c62d35b2f08d144b52b811a6daf3
Logan Honaker <147849501+LoganHonaker@users.noreply.github.com>
2024-11-06 23:46:04 -0500

    Added necessary controllers

--- Assignment5/api/dependencies/database.py
+++ Assignment5/api/dependencies/database.py
@@ -1,20 +1,19 @@
 from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, declarative_base
-from .config import conf
+from .config import conf  # Ensure conf is properly configured
 from urllib.parse import quote_plus
 
-
-
+# Build database URL with credentials
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{conf.user}:{quote_plus(conf.password)}@{conf.host}:{conf.port}/{conf.database}?charset=utf8mb4"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+
+# Create engine and session local
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Create a base class for the models
 Base = declarative_base()
 
-
+# Dependency to get the DB session
 def get_db():
     db = SessionLocal()
     try:
